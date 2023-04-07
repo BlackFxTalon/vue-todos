6d481d81b63071852795b36bd19a05f8fe9923cd
cvtProjects <a.vorontsov@cvt.ru>
2023-04-07 15:01:26 +0500

    first commit

---
+++ src/views/AboutView.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="about">
+    <h1>This is a vue.js todo web-application</h1>
+     <p>
+      The goal is to practice skills in vue.js basics by creating a vue.js todo web-application from scratch
+     </p>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    display: grid;
+    gap: 1rem;
+    align-items: center;
+    justify-content: center;
+    text-align: center;
+    margin: 10px auto;
+  }
+}
+</style>
