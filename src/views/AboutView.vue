64ed82ae4fbe2fb5b240ccf5ce298bf3d64f81bd
NinjiClaw <andyv@inbox.ru>
2023-04-10 12:10:17 +0500

    chore: :construction: changed description AboutView

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -4,6 +4,14 @@
      <p>
       The goal is to practice skills in vue.js basics by creating a vue.js todo web-application from scratch
      </p>
+     <p>List of TO-DO to improve functionality of of todo web-app:</p>
+
+      <ul>
+        <li><s>create todos on input enter keyup</s></li>
+        <li>drag and reorder todo items</li>
+        <li>animate the creation & deletion of todos</li>
+        <li>maybe fullstack web-app (track days)</li>
+      </ul>
   </div>
 </template>
 
